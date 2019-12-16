b62369af13daafb2375505612c68e639cbfaa291
hackerman234 <56136248+hackerman234@users.noreply.github.com>
2019-12-16 13:02:48 -0500

    Update mounter.sh

--- mounter.sh
+++ mounter.sh
@@ -4,8 +4,22 @@
 # mkdir /mnt/ramdisk
 # mount -t tmpfs -o size=500m tmpfs /mnt/ramdisk
 
-ram_check() {
+ram_check() { # Displays ram
     echo "[+] Displaying ram..."
     free -g
 }
 echo "$(ram_check)"
+
+make_dir_ramdisk() { # Makes the directory
+    echo "making dir ramdisk in /mnt/"
+    mkdir /mnt/ramdisk
+}
+
+mount() { # Function to mount the space the user wants
+    echo "[+] time to mount"
+    read -p "Enter how much space you would like to mount ex 500m: "
+    echo "[+] Mounting now..."
+    mount -t tmpfs -o size= tmpfs /mnt/ramdisk
+    echo '[+] Done!'
+}
+
